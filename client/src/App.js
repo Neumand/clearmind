c0c5f3eeb1cef656bcfb7ecd8a1b568e21025ac8
David Neuman <david.m.neuman@gmail.com>
2019-04-24 21:08:42 +0000

    Completed the initial setup of react-rails

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    users: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get('api/v1/users')
+      .then(response => {
+        console.log(response);
+        this.setState({
+          users: response.data,
+        });
+      })
+      .catch(error => console.log(error));
+  }
+
   render() {
+    const usersList = this.state.users.map(user => {
+      <li>{user.first_name} {user.last_name} - {user.email}</li>
+    })
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='App'>
+        <h1>Users:</h1>
+        <ul>
+          {usersList}
+        </ul>
       </div>
     );
   }
