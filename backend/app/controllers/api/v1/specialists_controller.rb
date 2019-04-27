47b08a34fac34295e393914bf762adc628e12032
David Neuman <david.m.neuman@gmail.com>
2019-04-27 18:26:45 +0000

    Add query for combining specialist data with their associated clinic and schedule

--- backend/app/controllers/api/v1/specialists_controller.rb
+++ backend/app/controllers/api/v1/specialists_controller.rb
@@ -2,8 +2,17 @@ class Api::V1::SpecialistsController < ApplicationController
 
   def index
     specialists = Specialist.all
+    specialist_details = []
 
-    render json: specialists
+    specialists.each do |specialist| 
+      object = {}
+      object["specialist"] = specialist
+      object["clinic"] = specialist.clinic
+      object["schedule"] = specialist.schedule
+      specialist_details << object
+    end
+
+    render json: specialist_details
   end
 
 end
