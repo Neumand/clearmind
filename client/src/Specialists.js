8739e2fb10bf3b9f0bb5ea2c32c8e41880c20c24
reshux <duruhanunsal@gmail.com>
2019-04-27 18:26:55 +0000

    Component refactor to reflect database changes

--- client/src/Specialists.js
+++ client/src/Specialists.js
@@ -1,22 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const Specialists = ({ specialists }) => {
-  const specList = specialists.map(specialist => {
+  const specList = specialists.map(input => {
     return (
-      <div className='col-md-4'>
-        <div className='card'>
-          <img src={specialist.image} className='card-img-top' alt='' />
-          <div className='card-body'>
-            <h5 className='card-title'>
-              {specialist.first_name} {specialist.last_name}
+      <div className="col-md-4">
+        <div className="card">
+          <img src={input.specialist.image} className="card-img-top" alt="" />
+          <div className="card-body">
+            <h5 className="card-title">
+              {input.specialist.first_name} {input.specialist.last_name}
             </h5>
-            <p className='card-text'>Expertise: {specialist.expertise}</p>
-            <a href='#' className='btn btn-dark'>
+            <p className="card-text">Expertise: {input.specialist.expertise}</p>
+            <a href="#" className="btn btn-dark">
               Book
             </a>
           </div>
-          <div className='card-footer'>
-            <small className='text-muted' />
+          <div className="card-footer">
+            <small className="text-muted">Clinic: {input.clinic.name} </small>
           </div>
         </div>
       </div>
@@ -25,9 +25,9 @@ const Specialists = ({ specialists }) => {
   return (
     <div>
       <h1>Our Specialists:</h1>
-      <div className='container'>
-        <div className='row'>
-          <div className='card-deck'>{specList}</div>
+      <div className="container">
+        <div className="row">
+          <div className="card-deck">{specList}</div>
         </div>
       </div>
     </div>
