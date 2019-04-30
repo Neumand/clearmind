d15e6c4fdd3bab8f2b426f16a80ba0ef0829571b
David Neuman <david.m.neuman@gmail.com>
2019-04-30 15:26:46 +0000

    Pass subset of information from apt table to front-end in order to successfully filter specialist's schedule based on booked appointments

--- backend/app/controllers/api/v1/appointments_controller.rb
+++ backend/app/controllers/api/v1/appointments_controller.rb
@@ -1,28 +1,38 @@
 class Api::V1::AppointmentsController < ApplicationController
-    def index
-        appointments = Appointment.all
-        render json: appointments
-    end
+	def index
+		appointments = Appointment.all
+		appointment_info = []
+		
+		appointments.each do |apt|
+			object = {}
+			object['specialist'] = apt.specialist_id
+			object['date_time'] = apt.date_time
+			object['cancelled'] = apt.cancelled
+			appointment_info << object
+		end
+			
+			render json: appointment_info
+	end
 
-    def create
-        appointment = Appointment.new
-        appointment.user_id = params[:user_id]
-        appointment.clinic_id = params[:clinic_id]
-        appointment.specialist_id = params[:specialist_id]
-        appointment.date_time = DateTime.parse(params[:date_time])
-        appointment.end_time = DateTime.parse(params[:date_time]) + 1.hour
-        appointment.session_details = params[:session_details]
-        appointment.cancelled = false
-        appointment.cancellation_reason = ""
-        appointment.save!
+	def create
+			appointment = Appointment.new
+			appointment.user_id = params[:user_id]
+			appointment.clinic_id = params[:clinic_id]
+			appointment.specialist_id = params[:specialist_id]
+			appointment.date_time = DateTime.parse(params[:date_time])
+			appointment.end_time = DateTime.parse(params[:date_time]) + 1.hour
+			appointment.session_details = params[:session_details]
+			appointment.cancelled = false
+			appointment.cancellation_reason = ""
+			appointment.save!
 
-        render json: appointment
-    end
+			render json: appointment
+	end
 
-   def update
-        appointment = Appointment.find(params[:id])
-        appointment.cancelled = true
-        render json: appointment
-   end
-  
+	def update
+			appointment = Appointment.find(params[:id])
+			appointment.cancelled = true
+			render json: appointment
+	end
+
 end
