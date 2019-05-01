2d3e3fc1357490a7ac9b59c1b482720a02038a35
David Neuman <david.m.neuman@gmail.com>
2019-05-01 17:25:04 +0000

    Add confirmation page which pulls props from Specialist component to render correct session details.

---
+++ client/src/Confirmation.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Container, Row, Jumbotron, Table } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+const Confirmation = props => {
+  const {
+    bookedSpec,
+    clinic,
+    endTime,
+    location,
+    startTime,
+  } = props.location.state;
+  return (
+    <Container>
+      <Row>
+        <FontAwesomeIcon icon={faCheckCircle} size='6x' />
+      </Row>
+      <Row>
+        <Jumbotron>
+          <h1>Confirmed!</h1>
+          <p>
+            Your booking has been confirmed and you will be receiving a
+            confirmation via the selected option shortly.
+          </p>
+          <p>Please see below for details regarding your appointment:</p>
+          <h3>Session Details</h3>
+        <Table hover variant='success'>
+          <thead>
+            <tr>
+              <th>Specialist</th>
+              <th>Date</th>
+              <th>From</th>
+              <th>To</th>
+              <th colSpan='2'>Clinic</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{bookedSpec}</td>
+              <td>{startTime.toDateString()}</td>
+              <td>{startTime.toLocaleTimeString('en-CA')}</td>
+              <td>{endTime.toLocaleTimeString('en-CA')}</td>
+              <td>{clinic}</td>
+              <td>{location}</td>
+            </tr>
+          </tbody>
+        </Table>
+        </Jumbotron>
+      </Row>
+    </Container>
+  );
+};
+
+export default Confirmation;
