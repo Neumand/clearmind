b0b5d9f0b9e7b7f7d1f2fefdbdd8995d28e2572a
reshux <duruhanunsal@gmail.com>
2019-05-02 17:50:27 +0000

    Add link to resources on confirmation component

--- client/src/Confirmation.js
+++ client/src/Confirmation.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Row, Jumbotron, Table } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 const Confirmation = props => {
   const {
@@ -14,7 +15,7 @@ const Confirmation = props => {
   return (
     <Container>
       <Row>
-        <FontAwesomeIcon icon={faCheckCircle} size='6x' />
+        <FontAwesomeIcon icon={faCheckCircle} size="6x" />
       </Row>
       <Row>
         <Jumbotron>
@@ -25,27 +26,31 @@ const Confirmation = props => {
           </p>
           <p>Please see below for details regarding your appointment:</p>
           <h3>Session Details</h3>
-        <Table hover variant='success'>
-          <thead>
-            <tr>
-              <th>Specialist</th>
-              <th>Date</th>
-              <th>From</th>
-              <th>To</th>
-              <th colSpan='2'>Clinic</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{bookedSpec}</td>
-              <td>{startTime.toDateString()}</td>
-              <td>{startTime.toLocaleTimeString('en-CA')}</td>
-              <td>{endTime.toLocaleTimeString('en-CA')}</td>
-              <td>{clinic}</td>
-              <td>{location}</td>
-            </tr>
-          </tbody>
-        </Table>
+          <Table hover variant="success">
+            <thead>
+              <tr>
+                <th>Specialist</th>
+                <th>Date</th>
+                <th>From</th>
+                <th>To</th>
+                <th colSpan="2">Clinic</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{bookedSpec}</td>
+                <td>{startTime.toDateString()}</td>
+                <td>{startTime.toLocaleTimeString('en-CA')}</td>
+                <td>{endTime.toLocaleTimeString('en-CA')}</td>
+                <td>{clinic}</td>
+                <td>{location}</td>
+              </tr>
+            </tbody>
+          </Table>
+          <p>
+            If you would like some more information, please check our{' '}
+            <Link to="/resources">resources</Link>.
+          </p>
         </Jumbotron>
       </Row>
     </Container>
