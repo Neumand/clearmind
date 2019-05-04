63351c3227102d4c145404e0c21a4233b0a820d0
David Neuman <david.m.neuman@gmail.com>
2019-05-04 16:59:29 +0000

    Add react-iframe and basic/temp hide/show functionality for the chatbot

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import { Navbar } from 'react-bootstrap';
+import React, { Component } from 'react';
+import { Navbar, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faComment } from '@fortawesome/free-solid-svg-icons';
+import Iframe from 'react-iframe';
 
-const Footer = () => {
-  return (
-    <Navbar fixed='bottom'>
-      <iframe
-        allow='microphone;'
-        width='350'
-        height='430'
-        src='https://console.dialogflow.com/api-client/demo/embedded/406ad268-b7a1-4edb-96e5-112b68f7e514'
-      />
-    </Navbar>
-  );
-};
+class Footer extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      display: 'show',
+    };
+  }
+
+  handleDisplay = e => {
+    this.state.display === 'none'
+      ? this.setState({ display: '' })
+      : this.setState({ display: 'none' });
+  };
+
+  render() {
+    return (
+      <Navbar fixed='bottom'>
+        <Button
+          variant='light'
+          onClick={this.handleDisplay}
+          style={{ display: !this.state.display }}
+        >
+          <FontAwesomeIcon icon={faComment} size='3x' />
+        </Button>
+        <Iframe
+          allow='microphone;'
+          width='350'
+          height='430'
+          src='https://console.dialogflow.com/api-client/demo/embedded/406ad268-b7a1-4edb-96e5-112b68f7e514'
+          display={this.state.display}
+          onClick={this.handleDisplay}
+        />
+      </Navbar>
+    );
+  }
+}
 
 export default Footer;
