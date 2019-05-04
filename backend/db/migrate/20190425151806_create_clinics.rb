f6ba4e28d2234cf4f7bc29454b5e8e8892105a8b
reshux <duruhanunsal@gmail.com>
2019-05-04 17:34:00 +0000

    Schema update to display Clinic numbers correctly

--- backend/db/migrate/20190425151806_create_clinics.rb
+++ backend/db/migrate/20190425151806_create_clinics.rb
@@ -3,7 +3,7 @@ class CreateClinics < ActiveRecord::Migration[5.2]
     create_table :clinics do |t|
       t.string :name
       t.string :address
-      t.integer :phone_number
+      t.string :phone_number
       t.string :description
       t.string :latitude
       t.string :longitude
