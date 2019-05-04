84e3775ea766c839bc71cca28ed82b7bbd266c91
reshux <duruhanunsal@gmail.com>
2019-05-04 17:33:32 +0000

    Clinics page small re-design

--- client/src/Clinics.js
+++ client/src/Clinics.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import MapComponent from './Map';
-import { Container } from 'react-bootstrap';
+import { Container, Card } from 'react-bootstrap';
 
 class Clinics extends React.Component {
   constructor(props) {
@@ -15,47 +15,39 @@ class Clinics extends React.Component {
   render() {
     const clinicsList = this.props.clinics.map(clinic => {
       return (
-        <ul key={clinic.id}>
-          <div
-            className={
-              this.state.activeCard === clinic.name
-                ? 'card border-dark'
-                : 'card'
-            }
-            id={clinic.name}
-          >
-            <div className="card-body">
-              <h5 className="card-title">{clinic.name}</h5>
-              <p className="card-text">
-                {clinic.description} <br />
-              </p>
-              <p className="card-text">{clinic.address} </p>
-              <p className="card-text">{clinic.phone_number} </p>
-            </div>
-          </div>
-        </ul>
+        <Card
+          style={{ marginBottom: `2rem`, marginRight: `2rem` }}
+          id={clinic.name}
+          border={this.state.activeCard === clinic.name ? 'dark' : 'light'}
+        >
+          <Card.Body>
+            <Card.Title>{clinic.name}</Card.Title>
+            <Card.Text>
+              {clinic.description} <br /> <br /> {clinic.address}
+              {clinic.phone_number}
+            </Card.Text>
+          </Card.Body>
+        </Card>
       );
     });
 
     return (
-      <Container>
-        <br />
-        <div className="card mb-3">
-          <div className="row no-gutters">
-            <div className="col-md-4">
-              <div className="card-body">{clinicsList}</div>
-            </div>
-            <div className="col-md-8">
-              <div className="card">
-                <div className="card-body">
-                  <MapComponent
-                    isMarkerShown
-                    clinics={this.props.clinics}
-                    borderChanger={this.borderChanger}
-                  />
-                </div>
-              </div>
-            </div>
+      <Container style={{ marginTop: `5REM` }}>
+        <h1 style={{ paddingBottom: `2 rem` }}>Our clinics</h1>
+        <p>
+          You can click on the map marker to highlight that clinic's
+          information.
+        </p>
+        <div className="row no-gutters">
+          <div className="col-md-4">{clinicsList}</div>
+          <div className="col-md-8">
+            <Card>
+              <MapComponent
+                isMarkerShown
+                clinics={this.props.clinics}
+                borderChanger={this.borderChanger}
+              />
+            </Card>
           </div>
         </div>
       </Container>
