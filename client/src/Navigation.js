2b5c591301aa8cb03c6c7a8b804cd4012915af60
reshux <duruhanunsal@gmail.com>
2019-05-05 17:57:00 +0000

    NavBar appointments link redesign

--- client/src/Navigation.js
+++ client/src/Navigation.js
@@ -6,7 +6,7 @@ const Navigation = () => {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
       <Link to="/" className="navbar-brand">
-        <img src='/nav-logo.png' style={{width: `2em`, height: `auto`}} />
+        <img src="/nav-logo.png" style={{ width: `2em`, height: `auto` }} />
       </Link>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
@@ -32,20 +32,24 @@ const Navigation = () => {
         </Nav>
         {localStorage.getItem('jwt') ? (
           <Nav>
-            <Navbar.Text>
-              Welcome,{' '}
-              <Link to="/profile">{localStorage.getItem('user name')}</Link>!
+            <Navbar.Text>Signed in as: &nbsp;</Navbar.Text>
+            <Navbar.Text style={{ color: `white` }}>
+              {localStorage.getItem('user name')}&nbsp;&nbsp;
             </Navbar.Text>
+
+            <Link to="/profile" className="nav-link">
+              Appointments
+            </Link>
+
             <Link to="/logout" className="nav-link">
               Logout
             </Link>
           </Nav>
         ) : (
           <Nav>
-            <Link to="/login" className="nav-link">
+            <Link to="/login" className="nav-link" style={{ color: `white` }}>
               Login
             </Link>
-            <Navbar.Text>|</Navbar.Text>
             <Link to="/register" className="nav-link">
               Register
             </Link>
