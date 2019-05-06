e32447f39c6ce03a53f163d20af7322c5ea3799f
David Neuman <david.m.neuman@gmail.com>
2019-05-06 18:57:06 +0000

    Add home page content. Modify text-align for each of the vector paragraphs

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { Container, Row, Card, Col, Media, Button } from 'react-bootstrap';
+import { Container, Row, Col, Media, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faCalendarCheck,
@@ -21,16 +21,17 @@ const Home = () => {
         <Row style={{ alignItems: `center`, flexDirection: `column` }}>
           <img
             src='/logo.png'
+            alt='Clearmind logo'
             style={{ width: `15rem`, marginBottom: `-2rem` }}
           />
-          <h3 style={{ fontWeight: `bold` }}>
-            A platform to privately and securely grant access to professional
-            help to those in need.
-          </h3>
+          <h4 style={{ fontWeight: `bold` }}>
+            Facilitating access to mental health specialists for those seeking
+            professional help
+          </h4>
         </Row>
       </Container>
 
-      <Container style={{marginTop: `7rem`, marginBottom: `7rem`}}>
+      <Container style={{ marginTop: `7rem`, marginBottom: `7rem` }}>
         <Row>
           <Col>
             <Media>
@@ -40,7 +41,7 @@ const Home = () => {
                 className='align-self-start mr-3'
               />
               <Media.Body>
-                <h5>Simple booking</h5>
+                <h5 className='heading'>Simple Booking</h5>
                 <p>
                   Easily book a session with one of specialists in the comfort
                   of your home.
@@ -56,10 +57,10 @@ const Home = () => {
                 className='align-self-start mr-3'
               />
               <Media.Body>
-                <h5>Session management</h5>
+                <h5 className='heading'>Session Management</h5>
                 <p>
-                  Keep track of your sessions with email and SMS confirmations,
-                  and cancel an upcoming appointment with the click of a button.
+                  Keep track of your sessions with SMS confirmations, and cancel
+                  an upcoming appointment with the click of a button.
                 </p>
               </Media.Body>
             </Media>
@@ -72,7 +73,7 @@ const Home = () => {
                 className='align-self-start mr-3'
               />
               <Media.Body>
-                <h5>Chat assistance</h5>
+                <h5 className='heading'>Chat Assistance</h5>
                 <p>
                   Our integrated chatbot will answer your general questions and
                   allow you to book an appointment via messaging.
@@ -85,32 +86,49 @@ const Home = () => {
 
       {/* Vector section */}
       <Container>
-        <Row style={{marginBottom: `5rem`}}>
+        <Row style={{ marginBottom: `5rem` }}>
           <Col>
-            <img src='/best-self.png' style={{maxWidth: `100%`}} />
+            <img
+              src='/best-self.png'
+              alt='Person as superhero with cape'
+              style={{ maxWidth: `100%` }}
+            />
           </Col>
-          <Col className='vector-column'>
-            <h3 style={{ color: `#0C355C`, fontWeight: `bold` }}>
-              Your Best Self
-            </h3>
-            <p>Lorem ipsum descriptum maximum </p>
+          <Col className='vector-column' style={{ textAlign: `left` }}>
+            <h3 className='heading'>Your Best Self</h3>
+            <p>
+              Everyone deserves to be their best self. If you feel that your
+              mental health is being negatively affected in any way, shape or
+              form, you may consider seeking professional help. With a wide
+              range of expertise, our specialists can tailor to your specific
+              needs and help alleviate your pain.
+            </p>
             <Link to='/register'>
               <Button>Register now to talk to someone</Button>
             </Link>
           </Col>
         </Row>
         <Row>
-          <Col className='vector-column'>
-          <h3 style={{ color: `#0C355C`, fontWeight: `bold` }}>
-              Your Loved Ones Care
-            </h3>
-            <p>Lorem ipsum descriptum maximum </p>
+          <Col className='vector-column' style={{ textAlign: `right` }}>
+            <h3 className='heading'>Healthy Support System</h3>
+            <p>
+              Friends and family play an important role in the lives of an
+              individual with a mental illness. Additionally, stories shared
+              from real people dealing with mental illness can help reduce the
+              stigma surrounding it and remind those suffering that they are not
+              alone. Learn more about how you can help or hear from inspiring
+              individuals by browsing our articles.
+            </p>
             <Link to='/resources'>
-              <Button>Learn more about mental health</Button>
+              <Button>Explore our available resources</Button>
             </Link>
           </Col>
           <Col>
-          <img src='/friends-family.png' style={{maxWidth: `100%`}} />
+            <img
+              src='/friends-family.png'
+              alt='Friends and family waiting at airport'
+              style={{ maxWidth: `100%` }}
+            />
           </Col>
         </Row>
       </Container>
