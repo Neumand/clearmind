3fead130483eebf91253f857ee9d2443e213b2dd
reshux <duruhanunsal@gmail.com>
2019-05-08 14:26:55 +0000

    Add componentDidMount to scroll to top at component render

--- client/src/Confirmation.js
+++ client/src/Confirmation.js
@@ -5,63 +5,69 @@ import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 import './Confirmation.css';
 
-const Confirmation = props => {
-  const {
-    bookedSpec,
-    clinic,
-    endTime,
-    location,
-    startTime,
-  } = props.location.state;
+class Confirmation extends React.Component {
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
 
-  return (
-    <Container style={{ marginTop: `6rem` }}>
-      <Row>
-        <Jumbotron>
-          <FontAwesomeIcon
-            icon={faCheckCircle}
-            size="6x"
-            style={{ color: `#5cb85c`, textAlign: `center` }}
-          />
-          <h1>Confirmed!</h1>
-          <p>
-            Your booking has been confirmed and you will be receiving a
-            confirmation via text message shortly.
-          </p>
-          <p>Please see below for details regarding your appointment:</p>
-          <h3>Session Details</h3>
-          <Table>
-            <thead>
-              <tr>
-                <th>Specialist</th>
-                <th>Date</th>
-                <th>From</th>
-                <th>To</th>
-                <th colSpan="2">Clinic</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>{bookedSpec}</td>
-                <td>{startTime.toDateString()}</td>
-                <td>{startTime.toLocaleTimeString('en-CA')}</td>
-                <td>{endTime.toLocaleTimeString('en-CA')}</td>
-                <td>{clinic}</td>
-                <td>{location}</td>
-              </tr>
-            </tbody>
-          </Table>
-          <p>
-            If you would like some more information, please check our{' '}
-            <Link to="/resources">resources</Link>.
-          </p>
-          <Link to="/">
-            <Button>Home</Button>
-          </Link>
-        </Jumbotron>
-      </Row>
-    </Container>
-  );
-};
+  render() {
+    const {
+      bookedSpec,
+      clinic,
+      endTime,
+      location,
+      startTime,
+    } = this.props.location.state;
+
+    return (
+      <Container style={{ marginTop: `6rem` }}>
+        <Row>
+          <Jumbotron>
+            <FontAwesomeIcon
+              icon={faCheckCircle}
+              size="6x"
+              style={{ color: `#5cb85c`, textAlign: `center` }}
+            />
+            <h1>Confirmed!</h1>
+            <p>
+              Your booking has been confirmed and you will be receiving a
+              confirmation via text message shortly.
+            </p>
+            <p>Please see below for details regarding your appointment:</p>
+            <h3>Session Details</h3>
+            <Table>
+              <thead>
+                <tr>
+                  <th>Specialist</th>
+                  <th>Date</th>
+                  <th>From</th>
+                  <th>To</th>
+                  <th colSpan="2">Clinic</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{bookedSpec}</td>
+                  <td>{startTime.toDateString()}</td>
+                  <td>{startTime.toLocaleTimeString('en-CA')}</td>
+                  <td>{endTime.toLocaleTimeString('en-CA')}</td>
+                  <td>{clinic}</td>
+                  <td>{location}</td>
+                </tr>
+              </tbody>
+            </Table>
+            <p>
+              If you would like some more information, please check our{' '}
+              <Link to="/resources">resources</Link>.
+            </p>
+            <Link to="/">
+              <Button>Home</Button>
+            </Link>
+          </Jumbotron>
+        </Row>
+      </Container>
+    );
+  }
+}
 
 export default Confirmation;
