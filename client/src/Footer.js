4c91c930ac17e5ada1f29f61aac2d7f84a4ec794
David Neuman <david.m.neuman@gmail.com>
2019-05-08 15:10:59 +0000

    Cited vector images on home page

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -7,8 +7,11 @@ const Footer = () => {
   // If not, chatbox iframe and the access button is not rendered
 
   return (
-    <Navbar sticky id="footer">
-      <Navbar.Text>Clearmind © 2019</Navbar.Text>
+    <Navbar sticky id='footer'>
+      <Navbar.Text>
+        Clearmind © 2019 &nbsp; Illustrations by{' '}
+        <a href='https://icons8.com'>Ouch.pics</a>
+      </Navbar.Text>
     </Navbar>
   );
 };
